c392276c586c37ac6d1471da6f56e189aafbe21f
ankitx23 <ankitxydv@gmail.com>
2025-08-22 15:32:03 -0400

    update

---
+++ Linked_List/append.py
@@ -0,0 +1,35 @@
+#11 -> 22 -> 33 -> 55
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print (temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            self.tail.next = new_node
+            self.tail= new_node
+        self.length +=1
+        
+my_linked_list = LinkedList(None)
+my_linked_list.append(22)
+my_linked_list.append(33)
+my_linked_list.append(55)
+my_linked_list.print_list()
+
